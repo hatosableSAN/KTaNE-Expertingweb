b3a9320f7edcc62365a54df86fc599745ba4b4e8
Hide1048 <e195412@st.u-gakugei.ac.jp>
2021-12-04 18:14:14 +0900

    add:User.java,UserMnager.java,UserDAO.javaを作成しました

---
+++ src/main/java/beans/User.java
@@ -0,0 +1,53 @@
+//  自分が格納されているフォルダ名
+package beans;
+
+public class User {
+
+    // 属性
+    private String id = null; // UserID
+    private String password = null; // パスワード
+    private String password2 = null; // パスワード確認
+
+    // 初期値を引数に持ったコンストラクタ
+    public User(String id, String password, String password2) {
+
+        this.id = id;
+        this.password = password;
+        this.password2 = password2;
+        
+    }
+
+    // 初期値を引数に持たないコンストラクタ
+    // Java beansは初期値を持たないコンストラクタが必ず必要
+    public User() {
+    }
+
+    // setメソッド
+    // Java beansのsetメソッドはsetの後ろに続く文字列が必ず大文字であること
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPassword2(String password2) {
+        this.password = password2;
+    }
+
+    // getメソッド
+    // Java beansのgetメソッドはgetの後ろに続く文字列が必ず大文字であること
+    public String getId() {
+        return this.id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getPassword2() {
+        return this.password2;
+    }
+
+}
---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,128 @@
+
+//　自分が格納されているフォルダ名
+package dao;
+
+//  自分が格納されているフォルダの外にある必要なクラス
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import beans.User;
+
+public class UserDAO {
+
+    // 属性
+
+    // データベースの接続先アドレスを静的変数として記述
+    private final static String DRIVER_URL = "jdbc:mysql://localhost:3306/Schoolapp_db?characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9:00&rewriteBatchedStatements=true";
+
+    // データベース接続ドライバの名前を静的変数として記述
+    // Mysql5.系
+    // private final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    // Mysql8.系
+    private final static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+    // データベースのユーザー名 （デフォルトではroot）
+    private final static String USER_NAME = "root";
+
+    // データベースのユーザーのパスワード (デフォルトでは設定なし)
+    private final static String PASSWORD = "";
+
+    // データベースとの接続を行う
+    // データベースの接続情報を持ったConnectionオブジェクトを返す
+    public Connection createConnection() {
+        try {
+            Class.forName(DRIVER_NAME);
+            Connection con = DriverManager.getConnection(DRIVER_URL, USER_NAME, PASSWORD);
+            return con;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't Find JDBC Driver.\n");
+        } catch (SQLException e) {
+            System.out.println("Connect Error.\n");
+        }
+        return null;
+    }
+
+    // Connectionオブジェクトを使って、データベースとの接続を切断する
+    // 引数Connectionオブジェクト
+    public void closeConnection(Connection con) {
+
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
+
+    // 情報をデータベースに登録する
+    // 引数はStudentオブジェクトと、Connectionオブジェクト
+    public void registUser(User user, Connection connection) {
+
+        try {
+
+            // SQLコマンド
+            String sql = "insert into user values(?, ?)";
+
+            // SQLコマンドの実行
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            // SQLコマンドのクエッションマークに値を、1番目から代入する
+            stmt.setString(1, user.getId());
+            stmt.setString(2, user.getPassword());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+
+            // エラーが発生した場合、エラーの原因を出力する
+            e.printStackTrace();
+
+        } finally {
+        }
+    }
+
+
+/*
+    // 検索する
+    // 引数はStudentオブジェクトと、Connectionオブジェクト
+    public Record searchRecord(Record record, Connection connection) {
+
+        try {
+
+            // SQLコマンド
+            String sql = "select * from bRecord where name = '" + record.getName() + "' and tournament = '" + record.getTournament() + "'";
+
+            // SQLのコマンドを実行する
+            // 実行結果はrsに格納される
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            rs.first();
+
+            // rsからそれぞれの情報を取り出し、Studentオブジェクトに設定する
+            record.setTournament(rs.getString("tournament"));
+            record.setRound(rs.getString("round"));
+            record.setDate(rs.getString("date"));
+            record.setResult(rs.getString("result"));
+
+            // 終了処理
+            stmt.close();
+            rs.close();
+
+            // Studentオブジェクトを返す
+            return record;
+
+        } catch (SQLException e) {
+
+            // エラーが発生した場合、エラーの原因を出力し、nullオブジェクトを返す
+            e.printStackTrace();
+            return null;
+
+        } finally {
+        }
+    }
+*/
+
+}
