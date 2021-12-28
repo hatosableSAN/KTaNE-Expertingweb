2d4504ffa735170fe7ff108403c2d2900dd96997
Mochi-MA <e195410k@st.u-gakugei.ac.jp>
2021-12-28 10:14:09 +0900

    update:init.sql(local)の更新と外部キーを追加

--- docker/local/mysql/init/init.sql
+++ docker/local/mysql/init/init.sql
@@ -37,11 +37,140 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` varchar(15) NOT NULL,
+  `gender` INT NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(user_id) REFERENCES user(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `classes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `year` INT NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(user_id) REFERENCES user(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `members` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `class_id` INT NOT NULL,
+  `student_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(class_id) REFERENCES classes(id),
+  FOREIGN KEY(student_id) REFERENCES students(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `seating_arrangements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seating_arrangements` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `class_id` INT NOT NULL,
+  `created_date` DATETIME NOT NULL DEFAULT '2020-01-01 00:00',
+  `start_date` DATETIME NOT NULL DEFAULT '2020-01-01 00:00',
+  `end_date` DATETIME DEFAULT '2020-01-01 00:00',
+  `name` varchar(20) DEFAULT '',
+  `user_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(class_id) REFERENCES classes(id),
+  FOREIGN KEY(user_id) REFERENCES user(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `students_seating_arrangements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students_seating_arrangements` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `seating_arrangements_id` INT NOT NULL,
+  `student_id` varchar(15) NOT NULL,
+  `seat` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(seating_arrangements_id) REFERENCES seating_arrangements(id),
+  FOREIGN KEY(student_id) REFERENCES students(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `grades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(15) NOT NULL,
+  `lesson_id` int NOT NULL,
+  `attendance` tinyint(1) NOT NULL,
+  `red` int DEFAULT NULL,
+  `blue` int DEFAULT NULL,
+  `green` int DEFAULT NULL,
+  `comment` varchar(400) DEFAULT NULL,
+  `seat` int NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(student_id) REFERENCES students(id),
+  FOREIGN KEY(lesson_id) REFERENCES lessons(id)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `lessons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lessons` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `seating_arrangements_id` int NOT NULL,
+  `lesson_date` date NOT NULL,
+  `period_num` int NOT NULL,
+  `comment` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(seating_arrangements_id) REFERENCES seating_arrangements(id)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` VALUES ('11111','0'),('1234','1234'),('12345','12345'),('123456','o1234567'),('abcdef','1234abcd'),('aiueo','happy'),('e195403','abc12345'),('e195412g','L9uiEp7g'),('happy1','happyhappy1'),('happy2','happyhappy2'),('happy3','happyhappy3'),('happy4','happyhappy4'),('happy5','happyhappy5');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES ('11111',2,'学芸花子','happy1'),('11112',1,'学芸太郎','happy1'),('11113',2,'学芸花子1','happy1'),('11114',2,'学芸花子2','happy1'),('11115',1,'学芸太郎2','happy1'),('11116',2,'学芸花子4','happy1');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'テストクラス1-1',2020,'happy1'),(2,'テストクラス1-2',2021,'happy1');
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (1,1,'11111'),(2,1,'11112'),(3,1,'11113'),(4,1,'11115');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
