35fd94d825594dc44b49168fe734f132c25ac7ec
IamYSuzuki <e195406n@st.u-gakugei.ac.jp>
2022-01-03 00:07:30 +0900

    update:児童検索機能のservletファイルを更新しました

--- src/main/java/servlet/SearchStudent.java
+++ src/main/java/servlet/SearchStudent.java
@@ -52,41 +52,38 @@ public class SearchStudent extends HttpServlet {
         List<Student> list = new ArrayList<Student>();
         // List<Student> list = new ArrayList<Student>();
         StudentService service = new StudentService();
-        // String stu_name = request.getParameter("stu_name");
-        // String stu_gender = request.getParameter("stu_gender");
+        String stu_info = request.getParameter("stu_search"); //textboxの値
+        String select = request.getParameter("radiobutton"); //ラジオボタンどちらが押されたか
         // int stu_gender = Integer.parseInt(gender);
         // String stu_user = "ABC"; //今ログインしている教員ユーザ
         // String taikai_l = request.getParameter("taikai_l");
         // String taikai_k = request.getParameter("taikai_k");
-        // session.setAttribute("stu_id",stu_id);
-        // session.setAttribute("stu_id",stu_name);
-        // session.setAttribute("stu_id",stu_gender);
-        // session.setAttribute("stu_id",stu_user);
 
         String tourl = null;
-        if (stu_search.isEmpty()) { // 本当は良くないけど、テキストボックスが空だったら一覧表示
+        if (stu_info.isEmpty()) { //テキストボックスが空だったら一覧表示
             list = service.getStudent();
             request.setAttribute("List", list);
             System.out.println("Please full all");
             tourl = "/WEB-INF/classes/registClass.jsp";
         } else {
             // studentオブジェクトに情報を格納
-            String stu_id = null;
-            String stu_name = null;
+            if(select.equals("number")){//番号のラジオボタンが押された
+                String stu_id = stu_info;
+                String stu_name = null;
+            }else if(select.equals("name")){//名前のラジオボタンが押された
+                String stu_id = null;
+                String name = stu_info;
+            }
+            //String stu_id = null;
+            //String stu_name = null;
             int stu_gender = 0;
             String stu_user = null;
             Student student = new Student(stu_id, stu_name, stu_gender, stu_user);
+            list = service.getStudent(stu_info,select);//引数を付けます。stu_infoとselect
             // StudentMemo studentmemo = new StudentMemo(stu_id, stu_name,
             // stu_gender,stu_user);
             // HttpSession session = request.getSession();
-            // session.setAttribute("StudentMemo",studentmemo);
-            // session.setAttribute("Student", student);
 
-            // StudentManagerオブジェクトの生成
-            // StudentService service = new StudentService();
-
-            // 登録
-            // list = service.searchStudent(stu_search);
             request.setAttribute("List", list);
 
             // 成功画面を表示する
