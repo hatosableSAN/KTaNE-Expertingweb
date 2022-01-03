e612457b4a6f83b917604a9b3af5db5607478ea7
Hatosable_0609 <81506948+hatosableSAN@users.noreply.github.com>
2022-01-03 14:48:36 +0900

    Update addportplate.js

--- addportplate.js
+++ addportplate.js
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript">
 $(document).on("click", ".add", function() {
     $(this).parent().clone(true).insertAfter($(this).parent());
 });
