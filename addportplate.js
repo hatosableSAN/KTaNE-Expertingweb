e548544c0d4e3736a3f4f256e7ee1f262eb8a25e
Hatosable_0609 <81506948+hatosableSAN@users.noreply.github.com>
2022-01-03 15:13:54 +0900

    Update addportplate.js

--- addportplate.js
+++ addportplate.js
@@ -7,7 +7,7 @@ Serial:<input type="text" name="name" size="3">
 Stereo RCA:<input type="text" name="name" size="3">
         <input type="button" value="＋" class="add pluralBtn">
         <input type="button" value="－" class="del pluralBtn">"
-     ).parent().clone(true).insertAfter($(this).parent());
+     ).insertAfter($(this).parent());
 });
 $(document).on("click", ".del", function() {
     var target = $(this).parent();
