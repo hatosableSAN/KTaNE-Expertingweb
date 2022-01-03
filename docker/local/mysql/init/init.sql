156a352d70fff11cf4a7f6213984f60afcb69994
Mochi-MA <e195410k@st.u-gakugei.ac.jp>
2022-01-03 13:54:32 +0900

    update:init.sqlにデータベース名を追加

--- docker/local/mysql/init/init.sql
+++ docker/local/mysql/init/init.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `classes`
 --
+DROP DATABASE IF EXISTS schoolapp_db;
+CREATE DATABASE schoolapp_db;
+USE schoolapp_db;
 
 DROP TABLE IF EXISTS `classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
