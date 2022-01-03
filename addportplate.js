72e20a1b48f52d12dc59fce549d93f1ece7ec89d
Hatosable_0609 <81506948+hatosableSAN@users.noreply.github.com>
2022-01-03 15:13:26 +0900

    Update addportplate.js

--- addportplate.js
+++ addportplate.js
@@ -1,5 +1,13 @@
 $(document).on("click", ".add", function() {
-    $(this).parent().clone(true).insertAfter($(this).parent());
+    $("DVI-D:<input type="text" name="name" size="3">
+Parallel:<input type="text" name="name" size="3">
+PS/2:<input type="text" name="name" size="3">
+RJ-45:<input type="text" name="name" size="3">
+Serial:<input type="text" name="name" size="3">
+Stereo RCA:<input type="text" name="name" size="3">
+        <input type="button" value="＋" class="add pluralBtn">
+        <input type="button" value="－" class="del pluralBtn">"
+     ).parent().clone(true).insertAfter($(this).parent());
 });
 $(document).on("click", ".del", function() {
     var target = $(this).parent();
