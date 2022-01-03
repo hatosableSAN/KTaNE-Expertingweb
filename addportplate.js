edf806a7272daf99434fe57124cb25126aa3a790
Hatosable_0609 <81506948+hatosableSAN@users.noreply.github.com>
2022-01-03 15:17:16 +0900

    Update addportplate.js

--- addportplate.js
+++ addportplate.js
@@ -1,13 +1,6 @@
 $(document).on("click", ".add", function() {
-    $("DVI-D:<input type="text" name="name" size="3">
-Parallel:<input type="text" name="name" size="3">
-PS/2:<input type="text" name="name" size="3">
-RJ-45:<input type="text" name="name" size="3">
-Serial:<input type="text" name="name" size="3">
-Stereo RCA:<input type="text" name="name" size="3">
-        <input type="button" value="＋" class="add pluralBtn">
-        <input type="button" value="－" class="del pluralBtn">"
-     ).insertAfter($(this).parent());
+    $("DVI-D:<input type="text" name="name" size="3">Parallel:<input type="text" name="name" size="3">PS/2:<input type="text" name="name" size="3">RJ-45:<input type="text" name="name" size="3">Serial:<input type="text" name="name" size="3">Stereo RCA:<input type="text" name="name" size="3"><input type="button" value="＋" class="add pluralBtn">
+<input type="button" value="－" class="del pluralBtn">").insertAfter($(this).parent());
 });
 $(document).on("click", ".del", function() {
     var target = $(this).parent();
