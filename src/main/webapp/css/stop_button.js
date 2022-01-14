70f5271ec64eaffb67973082bae18a46d3b09abf
Mochi-MA <e195410k@st.u-gakugei.ac.jp>
2022-01-14 22:36:44 +0900

    update:座席配置更新に二重送信防止.jsを反映させましta

--- src/main/webapp/css/stop_button.js
+++ src/main/webapp/css/stop_button.js
@@ -3,8 +3,9 @@
     console.log("ボタン二重送信防止テスト");
     $(document).on("click", 'input[type="submit"]', function (event) {
         console.log("ボタン二重送信防止");
-        $(event.target).val('送信中...');
+        $(event.target).val('処理中...');
         $('form').find('input[type="submit"], button[type="submit"]').prop('disabled', 'true');
+        $('button').prop('disabled', 'true');
         $(event.target).parents("form").submit();
     });
 })();
